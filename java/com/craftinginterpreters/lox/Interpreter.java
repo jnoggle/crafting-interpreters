325a11b9728487507547dca5ca3278b6ea5ac54a
Justin Noggle <justin.noggle@gmail.com>
2025-06-15 21:01:41 -0500

    Add function support

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,10 +1,35 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
-  private Environment environment = new Environment();
+  final Environment globals = new Environment();
+
+  private Environment environment = globals;
+
+  Interpreter() {
+    globals.define(
+      "clock",
+      new LoxCallable() {
+        @Override
+        public int arity() {
+          return 0;
+        }
+
+        @Override
+        public Object call(Interpreter interpreter, List<Object> arguments) {
+          return (double) System.currentTimeMillis() / 1000.0;
+        }
+
+        @Override
+        public String toString() {
+          return "<native fn>";
+        }
+      }
+    );
+  }
 
   void interpret(List<Stmt> statements) {
     try {
@@ -58,7 +83,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     return null;
   }
 
-  private void executeBlock(List<Stmt> statements, Environment environment) {
+  void executeBlock(List<Stmt> statements, Environment environment) {
     Environment previous = this.environment;
     try {
       this.environment = environment;
@@ -74,6 +99,13 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Void visitExpressionStmt(Stmt.Expression stmt) {
     evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitFunctionStmt(Stmt.Function stmt) {
+    LoxFunction function = new LoxFunction(stmt, environment);
+    environment.define(stmt.name.lexeme, function);
     return null;
   }
 
@@ -99,6 +131,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
     return null;
+  }
+
+  @Override
+  public Void visitReturnStmt(Stmt.Return stmt) {
+    Object value = null;
+    if (stmt.value != null) value = evaluate(stmt.value);
+
+    throw new Return(value);
   }
 
   @Override
@@ -186,6 +226,37 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         throw new Error("not implemented yet");
     }
     // Unreachable.
+  }
+
+  @Override
+  public Object visitCallExpr(Expr.Call expr) {
+    Object callee = evaluate(expr.callee);
+    List<Object> arguments = new ArrayList<>();
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+
+    if (!(callee instanceof LoxCallable)) {
+      throw new RuntimeError(
+        expr.paren,
+        "Can only call functions and classes."
+      );
+    }
+
+    LoxCallable function = (LoxCallable) callee;
+
+    if (arguments.size() != function.arity()) {
+      throw new RuntimeError(
+        expr.paren,
+        "Expected " +
+        function.arity() +
+        " arguments but got " +
+        arguments.size() +
+        "."
+      );
+    }
+
+    return function.call(this, arguments);
   }
 
   @Override
